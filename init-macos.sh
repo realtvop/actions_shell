6e07548bbab2ff6b176692a0bc669960836d2f50
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2023-12-11 23:10:26 +0800

    Update init-macos.sh

--- init-macos.sh
+++ init-macos.sh
@@ -6,7 +6,10 @@
 # wait
 sleep 10
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install ttyd
+
 # 运行
-nohup ./ttyd -i 127.0.0.1 bash &>/dev/null & disown
+nohup ttyd -i 127.0.0.1 bash &>/dev/null & disown
 nohup ./cloudflared-macos tunnel --url http://127.0.0.1:7681 &> ./cf.log & disown
 tail -f ./cf.log
